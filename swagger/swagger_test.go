6808f3314949ea746a307ec07f4ad5f92a88ffc8
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2022-06-22 19:42:15 +0300

    improve swagger generator

--- swagger/swagger_test.go
+++ swagger/swagger_test.go
@@ -1,90 +1,84 @@
 package swagger_test
 
 import (
-	"encoding/json"
-	"fmt"
-	"strings"
-	"testing"
+    "encoding/json"
+    "fmt"
+    "strings"
+    "testing"
 
-	"github.com/clubpay/ronycontrib/swagger"
-	"github.com/clubpay/ronykit/desc"
-	"github.com/clubpay/ronykit/std/gateway/fasthttp"
+    "github.com/clubpay/ronycontrib/swagger"
+    "github.com/clubpay/ronykit/desc"
+    "github.com/clubpay/ronykit/std/gateway/fasthttp"
 )
 
 type sampleReq struct {
-	X string   `json:"x"`
-	Y string   `json:"y"`
-	Z int64    `json:"z"`
-	W []string `json:"w"`
-}
-
-func (x sampleReq) Marshal() ([]byte, error) {
-	return json.Marshal(x)
+    X string   `json:"x"`
+    Y string   `json:"y"`
+    Z int64    `json:"z"`
+    W []string `json:"w"`
 }
 
 type subRes struct {
-	Some    string `json:"some"`
-	Another []byte `json:"another"`
+    Some    string `json:"some"`
+    Another []byte `json:"another"`
 }
 
 type sampleRes struct {
-	Out1 int      `json:"out1"`
-	Out2 string   `json:"out2"`
-	Sub  subRes   `json:"sub"`
-	Subs []subRes `json:"subs"`
-}
-
-func (x sampleRes) Marshal() ([]byte, error) {
-	return json.Marshal(x)
+    Out1 int      `json:"out1"`
+    Out2 string   `json:"out2"`
+    Sub  subRes   `json:"sub"`
+    Subs []subRes `json:"subs"`
 }
 
 type sampleError struct {
-	Code        int    `json:"code" swag:"enum:504,503"`
-	Description string `json:"description"`
+    Code        int    `json:"code" swag:"enum:504,503"`
+    Description string `json:"description"`
 }
 
-func (x sampleError) Marshal() ([]byte, error) {
-	return json.Marshal(x)
+type anotherRes struct {
+    subRes
+    Out1 int    `json:"out1"`
+    Out2 string `json:"out2"`
 }
 
 var testService = (&desc.Service{
-	Name:         "testService",
-	PreHandlers:  nil,
-	PostHandlers: nil,
+    Name:         "testService",
+    PreHandlers:  nil,
+    PostHandlers: nil,
 }).
-	AddContract(
-		desc.NewContract().
-			AddSelector(fasthttp.Selector{
-				Method: fasthttp.MethodGet,
-				Path:   "/some/:x/:y",
-			}).
-			SetInput(&sampleReq{}).
-			SetOutput(&sampleRes{}).
-			AddPossibleError(404, "ITEM1", &sampleError{}).
-			AddPossibleError(504, "SERVER", &sampleError{}).
-			SetHandler(nil),
-	).
-	AddContract(
-		desc.NewContract().
-			AddSelector(fasthttp.Selector{
-				Method: fasthttp.MethodPost,
-				Path:   "/some/:x/:y",
-			}).
-			SetInput(&sampleReq{}).
-			SetOutput(&sampleRes{}).
-			SetHandler(nil),
-	)
+        AddContract(
+            desc.NewContract().
+                    AddSelector(fasthttp.Selector{
+                        Method: fasthttp.MethodGet,
+                        Path:   "/some/:x/:y",
+                    }).
+                SetInput(&sampleReq{}).
+                SetOutput(&sampleRes{}).
+                AddPossibleError(404, "ITEM1", &sampleError{}).
+                AddPossibleError(504, "SERVER", &sampleError{}).
+                SetHandler(nil),
+        ).
+        AddContract(
+            desc.NewContract().
+                    AddSelector(fasthttp.Selector{
+                        Method: fasthttp.MethodPost,
+                        Path:   "/some/:x/:y",
+                    }).
+                SetInput(&sampleReq{}).
+                SetOutput(&anotherRes{}).
+                SetHandler(nil),
+        )
 
 func TestNewSwagger(t *testing.T) {
-	sg := swagger.NewSwagger("TestTitle", "v0.0.1", "")
-	sg.WithTag("json")
+    sg := swagger.NewSwagger("TestTitle", "v0.0.1", "")
+    sg.WithTag("json")
 
-	sb := &strings.Builder{}
-	err := sg.WriteTo(sb, testService)
-	if err != nil {
-		t.Fatal(err)
-	}
+    sb := &strings.Builder{}
+    err := sg.WriteTo(sb, testService)
+    if err != nil {
+        t.Fatal(err)
+    }
 
-	x, _ := json.MarshalIndent(json.RawMessage(sb.String()), "", "   ")
-	fmt.Println(string(x))
+    x, _ := json.MarshalIndent(json.RawMessage(sb.String()), "", "   ")
+    fmt.Println(string(x))
 }
